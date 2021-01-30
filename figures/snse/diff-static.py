c6c08540da6556e70b8c0305c70e1da37a6e4afd
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2021-01-30 10:32:39 -0500

    Downsampling snse images

--- figures/snse/diff-static.py
+++ figures/snse/diff-static.py
@@ -5,19 +5,21 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import numpy as np
 from iris import DiffractionDataset
+from skimage.filters import gaussian
 from matplotlib.ticker import FixedFormatter, FixedLocator
 from plotutils import FIGURE_WIDTH, ImageGrid, tag_axis
 from skued import autocenter
 
 INPUT = Path("data") / "snse"
+DOWNSAMPLING = 4
 
 with DiffractionDataset(INPUT / "static.hdf5") as source:
-    mask_exf = source.valid_mask
-    im_exf = source.diff_data(0)
+    mask_exf = source.valid_mask[::DOWNSAMPLING, ::DOWNSAMPLING]
+    im_exf = source.diff_data(0)[::DOWNSAMPLING, ::DOWNSAMPLING]
 
 with DiffractionDataset(INPUT / "overnight4.hdf5") as source:
-    mask_umt = source.valid_mask
-    im_umt = source.diff_eq()
+    mask_umt = source.valid_mask[::DOWNSAMPLING, ::DOWNSAMPLING]
+    im_umt = source.diff_eq()[::DOWNSAMPLING, ::DOWNSAMPLING]
 
 fig = plt.figure(figsize=(FIGURE_WIDTH, FIGURE_WIDTH / 2))
 grid = ImageGrid(fig, 111, nrows_ncols=(1, 2), cbar_location="top")
@@ -30,7 +32,7 @@ centers = list()
 for im, mask in zip([im_umt, im_exf], [mask_umt, mask_exf]):
     r, c = autocenter(im, mask).astype(np.int)
     centers.append((r, c))
-    side_lengths.append(floor(min([c, abs(c - 2048), r, abs(r - 2048)])))
+    side_lengths.append(floor(min([c, abs(c - im.shape[1]), r, abs(r - im.shape[0])])))
 side_length = min(side_lengths)
 
 for ax, (r, c), im, mask, vmax, crop, label in zip(
@@ -52,6 +54,7 @@ for ax, (r, c), im, mask, vmax, crop, label in zip(
     im = im[xs, ys]
     mask = mask[xs, ys]
     im = np.maximum(im, 0)
+    im = gaussian(im, sigma=4 / DOWNSAMPLING)
 
     m = ax.imshow(
         im, vmin=0, vmax=vmax, cmap="magma", extent=[0, im.shape[1], im.shape[0], 0]
@@ -60,7 +63,7 @@ for ax, (r, c), im, mask, vmax, crop, label in zip(
     ax.xaxis.set_visible(False)
     ax.yaxis.set_visible(False)
 
-    width = 170
+    width = 170 / DOWNSAMPLING
     height = 1.2 * im.shape[0] / 2
     x, y = im.shape[1] // 2 - width / 2, 0
 
@@ -71,7 +74,7 @@ for ax, (r, c), im, mask, vmax, crop, label in zip(
         edgecolor="k",
         facecolor="w",
     )
-    move_in = 6  # needs to be pixel-perfect. Adjusted for 600 DPI
+    move_in = 1  # needs to be pixel-perfect. Adjusted for 600 DPI
     crossover_patch = mpatches.Rectangle(
         xy=(x + move_in, y - 10),
         width=width - 2 * move_in,
