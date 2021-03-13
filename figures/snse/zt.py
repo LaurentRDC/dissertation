f46a9d264a519a8d3775a6f9521601192c67ae06
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2021-03-13 13:28:26 -0500

    Added zt plot

---
+++ figures/snse/zt.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from plotutils import discrete_colors
+from pathlib import Path
+
+DATADIR = Path("data") / "snse"
+
+fig, ax = plt.subplots(1, 1, figsize=(4, 3))
+
+T, c, b, a = np.loadtxt(DATADIR / "zt.csv", delimiter=",", comments="#", unpack=True)
+
+for color, axis, label, marker in zip(
+    discrete_colors(3), [a, b, c], ["a-axis", "b-axis", "c-axis"], ["o", "s", "^"]
+):
+    ax.errorbar(
+        T,
+        axis,
+        yerr=0.15 * axis,
+        label=label,
+        ecolor=color,
+        color=color,
+        linestyle="none",
+        marker=marker,
+    )
+
+ax.legend(loc="upper left", edgecolor="none")
+ax.set_xlabel("Temperature [K]")
+ax.set_ylabel("ZT [a.u.]")
+ax.set_ylim([-0.1, 3])
+ax.axvline(x=810, color="k", linestyle="--", linewidth=1)
+ax.text(x=810, y=3.1, s="$T_c$", ha="center")
+offset = 50
+ax.text(x=810 - offset, y=3.1, s="$\leftarrow Pnma$", ha="right")
+ax.text(x=810 + offset, y=3.1, s=r"$Cmcm \rightarrow$", ha="left")
+
+plt.tight_layout()
