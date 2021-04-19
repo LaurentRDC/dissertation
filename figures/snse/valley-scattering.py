2aa15b55791eba4212b049465aa03f1da92e30e7
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2021-04-19 08:28:33 -0400

    Valley scattering plot

--- figures/snse/valley-scattering.py
+++ figures/snse/valley-scattering.py
@@ -1,145 +1,202 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import itertools as it
+from math import sqrt
+from functools import reduce
+from collections import namedtuple
+from matplotlib.patches import Rectangle, Circle, FancyArrowPatch
 import matplotlib.ticker as ticker
-from matplotlib.patches import FancyArrowPatch
-from pathlib import Path
-from dissutils import tag_axis, discrete_colors
+import matplotlib.cm as cm
+from skimage.filters import gaussian
+import numpy as np
+import matplotlib.colors as cl
+from dissutils import LARGE_FIGURE_WIDTH, discrete_colors, tag_axis
 
-BAND_COLOR, COLOR_TAU1, COLOR_TAU2 = discrete_colors(3)
+ARROWSTYLE = dict(arrowstyle="-|>", zorder=np.inf, mutation_scale=5, fc="k", ec="k")
 
+_, PURPLE, _, _, _, _, YELLOW = discrete_colors(7)
+ELEC_CMAP = cl.LinearSegmentedColormap.from_list("", [YELLOW, "w"])
+HOLE_CMAP = cl.LinearSegmentedColormap.from_list("", ["w", PURPLE])
 
-def cb1(k, T):
-    t = T / 600 - 1
-    kc = 0.66 + 0.05 * t
-    return 15 * (k - kc) ** 2 + 0.6 + t / 2
+R60deg = np.array([[1 / 2, -sqrt(3) / 2], [sqrt(3) / 2, 1 / 2]])
 
+ky, kz = np.meshgrid(*[np.linspace(-1, 1, num=512)] * 2)
+hole_region = ky < -kz
+elec_region = np.logical_not(hole_region)
 
-def cb2(k, T):
-    t = T / 600 - 1
-    return 5 * k ** 2 + 0.6 - t / 5
-
-
-def vb1(k, T):
-    t = T / 600 - 1
-    kc = 0.66 + 0.05 * t
-    return -20 * (k - kc) ** 2 + t / 4 - 0.35
+elec_energy = np.zeros_like(ky)
+hole_energy = np.zeros_like(ky)
 
 
-def vb2(k, T):
-    t = T / 600
-    kc = -0.75
+for (cy, cz), height in zip(
+    [(2 / 3, 0), (0, 0), (0, 2 / 3)],
+    [1, 0.8, 0.7],
+):
+    elec_energy[
+        np.argmin(np.abs(kz[:, 0] - cz)), np.argmin(np.abs(ky[0, :] - cy))
+    ] = height
 
-    return -1000 * (k - kc) ** 4 + (15 / t) * (k - kc) ** 2 + t / 10 - 0.4
+elec_energy[:] = gaussian(elec_energy, sigma=40)
+elec_energy *= -1
+elec_energy -= elec_energy.min()
 
+elec_energy /= elec_energy.max()
+elec_energy[hole_region] = elec_energy.max()
 
-def gm(k, T):
-    t = T / 600
-    kc = 0
-    return -0.9 * (k - kc) ** 2 - 0.9 + t / 15
+for (cy, cz), height in zip(
+    [(-2 / 3, 0), (0, 0)],
+    [0.7, 0.6],
+):
+    hole_energy[
+        np.argmin(np.abs(kz[:, 0] - cz)), np.argmin(np.abs(ky[0, :] - cy))
+    ] = height
+hole_energy[:] = gaussian(hole_energy, sigma=40)
+hole_energy /= hole_energy.max()
 
+# Pudding mold band near 3/4Z
+near_Z = np.sqrt(ky ** 2 + (kz + 3 / 4) ** 2)
+pudding_mold_energy = np.zeros_like(hole_energy)
+pudding_mold_energy[near_Z < 0.02] = -1.5
+pudding_mold_energy[np.isclose(near_Z, 0.13, rtol=5e-2)] = 1
+# pudding_mold_energy[near_Z < 0.19] = 0.15
+pudding_mold_energy[:] = gaussian(pudding_mold_energy, sigma=20)
+pudding_mold_energy /= pudding_mold_energy.max()
 
-figure, ax = plt.subplots(1, 1, figsize=(3, 2))
+hole_energy += pudding_mold_energy
+hole_energy -= hole_energy.max()
+hole_energy[elec_region] = hole_energy.min()
 
-k = np.linspace(-1, 1, 256)
-ax.plot(k, cb1(k, T=300), color=BAND_COLOR)
-ax.plot(k, vb1(k, T=300), color=BAND_COLOR)
-ax.plot(k, cb2(k, T=300), color=BAND_COLOR)
-ax.plot(k, vb2(k, T=300), color=BAND_COLOR)
-ax.plot(k, gm(k, T=300), color=BAND_COLOR)
-
-ax.set_ylim([-1, 1.4])
-ax.set_xlim([k.min(), k.max()])
-ax.xaxis.set_major_locator(ticker.FixedLocator(locs=[-1, 0, 1]))
-ax.xaxis.set_major_formatter(ticker.FixedFormatter(["$Z$", "$\Gamma$", "$Y$"]))
-ax.yaxis.set_visible(False)
-
-arrow_kwds = dict(
-    shrinkA=0, shrinkB=0, mutation_scale=10, clip_on=False, arrowstyle="-|>"
+figure, (ax_elec1, ax_elec2, ax_elec3) = plt.subplots(
+    1, 3, figsize=(LARGE_FIGURE_WIDTH, 0.4 * LARGE_FIGURE_WIDTH)
 )
 
+cax = ax_elec1.inset_axes(bounds=[-0.03, 0.1, 0.05, 0.8])
 
-ax.add_patch(
-    FancyArrowPatch(
-        posA=(-0.35, gm(-0.35, 300)),
-        posB=(-0.35, cb2(-0.35, 300)),
-        ec="r",
-        fc="r",
-        **arrow_kwds
+for i, ax in enumerate([ax_elec1, ax_elec2, ax_elec3]):
+
+    ax.axis("off")
+    ax.set_aspect(1)
+
+    elec_colors = [ELEC_CMAP(bc) for bc in np.linspace(0, 1, num=7)]
+    hole_colors = [HOLE_CMAP(bc) for bc in np.linspace(0, 1, num=len(elec_colors))]
+    hole_colors[0] = elec_colors[-1] = (0, 0, 0, 0)
+
+    me = ax.contourf(ky, kz, elec_energy, colors=elec_colors, levels=len(elec_colors))
+    mh = ax.contourf(ky, kz, hole_energy, colors=hole_colors, levels=len(hole_colors))
+
+    # Split hole/electron regions
+    y = np.linspace(-1, 1, num=1024)
+    ax.plot(y, -y, "k", clip_on=True, linewidth=1)
+
+    ax.add_patch(
+        Rectangle(
+            xy=(-1, -1),
+            width=2,
+            height=2,
+            fc="none",
+            ec="k",
+            clip_on=False,
+            zorder=np.inf,
+        )
     )
+
+    for (x, y), label in zip(
+        [(0, 0), (1, 0), (0, 1), (1, 1), (-1, 0), (0, -1), (-1, -1), (-1, 1), (1, -1)],
+        ["$\Gamma$", "$Y$", "$Z$", "$T$"] + [""] * 5,
+    ):
+        ax.add_patch(
+            Circle(
+                xy=(x, y), radius=0.03, edgecolor="None", facecolor="k", zorder=np.inf
+            )
+        )
+
+        # only annotate the first subplot
+        if i == 0:
+            ax.annotate(
+                xy=(x, y),
+                ha="left",
+                va="bottom",
+                text=label,
+                xytext=np.array((x, y)) + np.array([0.06, 0.06]),
+            )
+
+    ax.text(x=0.9, y=0.9, s="e$^{-}$", va="top", ha="right")
+    ax.text(x=-0.9, y=-0.9, s="h$^{+}$", va="bottom", ha="left")
+
+cmap_colors = hole_colors + [(0.7, 0.7, 0.7, 0.7)] + elec_colors
+mappable = cm.ScalarMappable(
+    norm=cl.Normalize(vmin=-1, vmax=1),
+    cmap=cl.ListedColormap(name="", colors=cmap_colors),
 )
-ax.text(
-    x=-0.35,
-    y=(gm(-0.35, 300) + cb2(-0.35, 300)) / 2,
-    s="$\gamma$",
-    color="red",
-    ha="left",
-    va="center",
-    transform=ax.transData,
-)
+plt.colorbar(mappable=mappable, cax=cax)
+cax.yaxis.set_major_locator(ticker.FixedLocator([0]))
+cax.yaxis.set_major_formatter(ticker.FixedFormatter(["$E_f$"]))
+cax.yaxis.tick_left()
+cax.yaxis.set_label_position("left")
 
 
-# Intravalley relaxation
-ax.add_patch(
-    FancyArrowPatch(
-        posA=(-0.35, cb2(-0.35, 300)),
-        posB=(0.2, cb2(0.2, 300)),
-        ec=COLOR_TAU1,
-        fc=COLOR_TAU1,
-        **arrow_kwds
+# intravalley scattering
+for xy, v in zip(
+    map(np.asarray, [(0, 0), (2 / 3, 0), (-2 / 3, 0), (0, 2 / 3)]),
+    [(1 / 2, 1 / 2)] + [(0, 3 / 4)] * 6,
+):
+
+    for n in range(0, 6):
+        start = 0.3 * reduce(lambda m1, m2: m1 @ m2, n * [R60deg], R60deg) @ np.array(v)
+        ax_elec2.add_patch(FancyArrowPatch(posA=start + xy, posB=xy, **ARROWSTYLE))
+
+# Pudding mold hole band is special
+for n in range(0, 6):
+    xy = (0, -3 / 4)
+    start_out = (
+        0.3
+        * reduce(lambda m1, m2: m1 @ m2, n * [R60deg], R60deg)
+        @ np.array([3 / 4, 0])
     )
-)
-ax.text(
-    x=0,
-    y=(cb2(-0.35, 300) + cb2(0.2, 300)) / 2,
-    s="$\\tau_1$",
-    color=COLOR_TAU1,
-    ha="center",
-    va="bottom",
-    transform=ax.transData,
-)
-
-ax.add_patch(
-    FancyArrowPatch(
-        posA=(0.2, cb2(0.2, 300)),
-        posB=(-0.2, cb2(-0.2, 300)),
-        ec=COLOR_TAU1,
-        fc=COLOR_TAU1,
-        **arrow_kwds
+    start_in = (
+        0.2
+        * reduce(lambda m1, m2: m1 @ m2, n * [R60deg], R60deg)
+        @ np.array([0, 3 / 4])
     )
-)
-
-ax.add_patch(
-    FancyArrowPatch(
-        posA=(-0.2, cb2(-0.2, 300)),
-        posB=(0, cb2(0, 300)),
-        ec=COLOR_TAU1,
-        fc=COLOR_TAU1,
-        **arrow_kwds
+    ax_elec2.add_patch(
+        FancyArrowPatch(
+            posA=3 / 2 * start_out + xy, posB=1 / 2 * start_out + xy, **ARROWSTYLE
+        )
     )
-)
-
-# Intervalley relaxation
-ax.add_patch(
-    FancyArrowPatch(
-        posA=(0, cb2(0, 300)),
-        posB=(0.66, cb1(0.66, 300)),
-        ec=COLOR_TAU2,
-        fc=COLOR_TAU2,
-        zorder=np.inf,
-        **arrow_kwds
+    ax_elec2.add_patch(
+        FancyArrowPatch(posA=xy, posB=start_in + xy, shrinkA=0, **ARROWSTYLE)
     )
+
+# Intervalley scattering
+extrema = namedtuple("Extrema", ["yz", "E"])
+elec_endpoints = [
+    extrema(yz=(0, 2 / 3), E=2),
+    extrema(yz=(0, 0), E=1),
+    extrema(yz=(2 / 3, 0), E=0),
+]
+hole_endpoints = [
+    extrema(yz=(0, -3 / 4), E=2),
+    extrema(yz=(0, 0), E=0),
+    extrema(yz=(-2 / 3, 0), E=1),
+]
+
+valid_elec_pathway = lambda t: t[0].E > t[1].E  # Electrons scatter down
+valid_hole_pathway = lambda t: t[0].E < t[1].E  # Holes scatter up
+
+# Filter to remove pathways that go up in energy
+elec_pathways = list(filter(valid_elec_pathway, it.product(elec_endpoints, repeat=2)))
+hole_pathways = list(filter(valid_hole_pathway, it.product(hole_endpoints, repeat=2)))
+
+# Electrons: positive quadrant
+for (start, _), (end, _) in elec_pathways + hole_pathways:
+    ax_elec3.add_patch(
+        FancyArrowPatch(posA=start, posB=end, shrinkA=5, shrinkB=5, **ARROWSTYLE)
+    )
+
+tag_kwds = dict(x=0.12, y=0.88)
+tag_axis(ax_elec1, "a)", **tag_kwds)
+tag_axis(ax_elec2, "b)", **tag_kwds)
+tag_axis(ax_elec3, "c)", **tag_kwds)
+
+plt.subplots_adjust(
+    top=1.0, bottom=0.006, left=0.056, right=1.0, hspace=0.135, wspace=0.052
 )
-ax.text(
-    x=0.33,
-    y=(cb2(0, 300) + cb1(0.66, 300)) / 2,
-    s="$\\tau_2$",
-    color=COLOR_TAU2,
-    ha="center",
-    va="bottom",
-    transform=ax.transData,
-)
-
-
-ax.set_ylabel("$\epsilon(\mathbf{k})$")
-
-plt.tight_layout()
