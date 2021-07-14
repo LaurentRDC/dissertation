e468d72979107b4ffd16187b2fe2403a59536b5c
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2021-07-14 09:54:26 -0400

    Shuffling of figures

--- figures/snse/performance.py
+++ figures/snse/performance.py
@@ -5,54 +5,45 @@ from pathlib import Path
 
 DATADIR = Path("data") / "snse"
 
-fig, (ax_S, ax_klat) = plt.subplots(2, 1, figsize=(MEDIUM_FIGURE_WIDTH, 4), sharex=True)
-ax_S.set_ylabel("$s$ [$\mu$V/K]")
-ax_S.xaxis.set_visible(False)
-ax_klat.set_ylabel("$\kappa_l$ [W/m/K]")
-
-T, S_c, S_b, S_a = np.loadtxt(
-    DATADIR / "seebeck.csv", delimiter=",", comments="#", unpack=True
+fig, (ax_ZT, ax_S, ax_klat) = plt.subplots(
+    3, 1, figsize=(MEDIUM_FIGURE_WIDTH, 4.5), sharex=True
 )
-for color, trace, label, marker in zip(
-    discrete_colors(3), [S_a, S_b, S_c], ["a-axis", "b-axis", "c-axis"], ["o", "s", "^"]
-):
-    ax_S.plot(
-        T,
-        trace,
-        label=label,
-        color=color,
-        linestyle="none",
-        marker=marker,
-    )
-ax_S.legend(ncol=3, loc="center", bbox_to_anchor=(0.5, 1.1), edgecolor="none")
 
-T, k_c, k_b, k_a = np.loadtxt(
-    DATADIR / "klat.csv", delimiter=",", comments="#", unpack=True
-)
-for color, trace, label, marker in zip(
-    discrete_colors(3), [k_a, k_b, k_c], ["a-axis", "b-axis", "c-axis"], ["o", "s", "^"]
+for ax, filepath, ylabel in zip(
+    [ax_ZT, ax_S, ax_klat],
+    [DATADIR / "zt.csv", DATADIR / "seebeck.csv", DATADIR / "klat.csv"],
+    ["$ZT$ [a.u.]", "$S$ [$\mu$V/K]", "$\kappa_l$ [W/m/K]"],
 ):
-    ax_klat.plot(
-        T,
-        trace,
-        label=label,
-        color=color,
-        linestyle="none",
-        marker=marker,
-    )
+    T, c, b, a = np.loadtxt(filepath, delimiter=",", comments="#", unpack=True)
+    ax.set_ylabel(ylabel)
+    for color, trace, label, marker in zip(
+        discrete_colors(3), [a, b, c], ["a-axis", "b-axis", "c-axis"], ["o", "s", "^"]
+    ):
+        ax.plot(
+            T,
+            trace,
+            label=label,
+            color=color,
+            linestyle="none",
+            marker=marker,
+        )
 
+ax_ZT.legend(edgecolor="none", loc="center", bbox_to_anchor=(0.3, 0.6))
 ax_klat.set_xlabel("Temperature [K]")
 
-for ax, label in zip([ax_S, ax_klat], ["a)", "b)"]):
+for ax, label, (y, va) in zip(
+    [ax_ZT, ax_S, ax_klat],
+    ["a)", "b)", "c)"],
+    [(0.88, "top"), (0.12, "bottom"), (0.12, "bottom")],
+):
     ax.axvline(x=810, color="k", linestyle="--", linewidth=0.5)
-    tag_axis(ax, label, y=0.12, verticalalignment="bottom")
+    tag_axis(ax, label, y=y, verticalalignment=va)
 
-ax_klat.axvline(x=810, color="k", linestyle="--", linewidth=0.5)
-ax_klat.text(x=810, y=1.15 * k_c.max(), s="$T_c$", ha="center")
+# Phase transition temperature
+_, zt_max = ax_ZT.get_ylim()
+ax_ZT.text(x=810, y=1.05 * zt_max, s="$T_c$", ha="center")
 offset = 50
-ax_klat.text(x=810 - offset, y=1.15 * k_c.max(), s="$\leftarrow Pnma$", ha="right")
-ax_klat.text(x=810 + offset, y=1.15 * k_c.max(), s=r"$Cmcm \rightarrow$", ha="left")
+ax_ZT.text(x=810 - offset, y=1.05 * zt_max, s="$\leftarrow Pnma$", ha="right")
+ax_ZT.text(x=810 + offset, y=1.05 * zt_max, s=r"$Cmcm \rightarrow$", ha="left")
 
-plt.subplots_adjust(
-    top=0.896, bottom=0.146, left=0.146, right=0.931, hspace=0.269, wspace=0.2
-)
+plt.tight_layout()
